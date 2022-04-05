1f71f8c8c63d16aa16dab918f770ec2edd325c63
Jeremy Pierson <jeremy@sobit.org>
2022-04-05 09:52:26 -0700

    comments added

--- randomcow.sh
+++ randomcow.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Random Number Generator using Cowsay
-# Requires Cowsay
+# Choose a random using RANDOM and cowsay
+# uses cowsay and figlet
+# accepts range as command line paramater. To select a random number from 1-25:
+# ./randomcow 25 
 
-#echo $[RANDOM%14+1] | cowsay
 
-echo $1
 
 clear && echo "Cow is thinking..." | figlet &&  sleep 3 && echo $[RANDOM%$1+1] | cowsay && echo "Mooooooo" | figlet
